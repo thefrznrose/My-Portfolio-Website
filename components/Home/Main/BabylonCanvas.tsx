5903fce588ffa3e4647f740f85983ce14b41a8c3
Shaun Rose <thefrznrose@gmail.com>
2025-01-07 16:51:02 -0800

    Re-structured the babylonCanvas file

--- components/Home/Main/BabylonCanvas.tsx
+++ components/Home/Main/BabylonCanvas.tsx
@@ -9,32 +9,39 @@ import {
 } from "@babylonjs/core";
 import { getMotif } from "@/functions/GetMotif";
 
+// Initialize the canvas function that returns engine and scene.
+function initializeCanvas(canvasRef: HTMLCanvasElement | null) {
+  if (!canvasRef) return { engine: null, scene: null };
+  // Initilizing the engine and scene.
+  const engine = new Engine(canvasRef, true);
+  const scene = new Scene(engine);
+  //Initializing the camera.
+  const camera = new ArcRotateCamera(
+    "camera",
+    Math.PI / 2,
+    Math.PI / 4, 
+    5, 
+    new Vector3(0, 0, 0), 
+    scene
+  );
+  camera.attachControl(canvasRef, true);
+  //Initializing the light.
+  const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+  light.intensity = 0.8;
+  const directionalLight = new DirectionalLight("dirLight", new Vector3(-1, -2, -1), scene);
+  directionalLight.intensity = 1.0;
+  // Return the engine and scene
+  return { engine, scene };
+}
+
+// Declaring the babylon canvas.
 const BabylonCanvas: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-
   useEffect(() => {
-    if (!canvasRef.current) return;
-
-    const engine = new Engine(canvasRef.current, true);
-    const scene = new Scene(engine);
-
-    const camera = new ArcRotateCamera(
-      "camera",
-      Math.PI / 2,
-      Math.PI / 4, 
-      5, 
-      new Vector3(0, 0, 0), 
-      scene
-    );
-    camera.attachControl(canvasRef.current, true);
-    
-    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-    light.intensity = 0.8;
-    const directionalLight = new DirectionalLight("dirLight", new Vector3(-1, -2, -1), scene);
-    directionalLight.intensity = 1.0;
-
-
-    // Load the motif
+    // Initializing the canvas.
+    const {engine, scene} = initializeCanvas(canvasRef.current)
+    if (!engine || !scene) return;
+    // Load a motif from the public directory.
     const loadMotif = async () => {
         try {
           const { motif, center } = await getMotif("1Y26.json", "0xcc2900", 0xff3300, scene);
@@ -44,25 +51,23 @@ const BabylonCanvas: React.FC = () => {
           console.error("Error loading motif:", error);
         }
       };
-
     loadMotif();
-
+    //Render loop.
     engine.runRenderLoop(() => {
       scene.render();
     });
-
+    // Event lister to handle resizing.
     const resizeHandler = () => {
       engine.resize();
     };
     window.addEventListener("resize", resizeHandler);
-
+    //Clean up after unmounting..
     return () => {
       scene.dispose();
       engine.dispose();
       window.removeEventListener("resize", resizeHandler);
     };
   }, []);
-
   return <canvas ref={canvasRef} style={{ width: "100%", height: "100%" }} />;
 };
 
